4d34168cb9fa77e928e230205dd82ee02fe1d02f
Sadrac <sadrac.santacruz@gmail.com>
2025-03-09 21:58:17 -0700

    hi

--- work.py
+++ work.py
@@ -6,4 +6,4 @@ medications_request = pd.read_csv('csv/cleaned_medication_request.csv')
 # medications_request = pd.merge(patients, conditions, on='PatientID', how='left')
 
 # Convert DataFrame to JSON and save it
-medications_request.to_json("json/patient_conditions.json", orient="records", indent=4)
+medications_request.to_json("json/cleaned_medication_request.json", orient="records", indent=4)
